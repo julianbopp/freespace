0e8d32508e81f0c9ea1e8f8bd405f72f69da3bef
julianbopp <julian.bopp@unibas.ch>
2023-03-17 13:33:41 +0100

    Print available capacity

--- Sources/freespace/freespace.swift
+++ Sources/freespace/freespace.swift
@@ -1,8 +1,19 @@
+import Foundation
+
 @main
 public struct freespace {
-    public private(set) var text = "Hello, World!"
 
     public static func main() {
-        print(freespace().text)
+        let fileURL = URL(fileURLWithPath:"/")
+        do {
+            let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityKey])
+            if let capacity = values.volumeAvailableCapacity{
+                print("Available capacity: \(capacity)")
+            } else {
+                print("Capacity is unavailable")
+            }
+        } catch {
+            print("Error retrieving capacity: \(error.localizedDescription)")
+        }
     }
 }
