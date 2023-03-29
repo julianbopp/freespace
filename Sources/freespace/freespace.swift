3dce3887afba672aecae30e24f860f44987527fe
julianbopp <julian.bopp@unibas.ch>
2023-03-29 12:13:31 +0200

    Update comments

--- Sources/freespace/freespace.swift
+++ Sources/freespace/freespace.swift
@@ -7,6 +7,24 @@ public struct freespace {
         let fileURL = URL(fileURLWithPath:"/")
         do {
             let values = try fileURL.resourceValues(forKeys: [.volumeAvailableCapacityKey])
+            /* 
+            Description of keys from Apple:
+
+            ------------------------------------------------------------------------    
+            static let volumeAvailableCapacityKey: URLResourceKey
+                Key for the volume’s available capacity in bytes (read-only).
+            static let volumeAvailableCapacityForImportantUsageKey: URLResourceKey
+                Key for the volume’s available capacity in bytes for storing important resources (read-only).
+            static let volumeAvailableCapacityForOpportunisticUsageKey: URLResourceKey
+                Key for the volume’s available capacity in bytes for storing nonessential resources (read-only).
+            static let volumeTotalCapacityKey: URLResourceKey
+                Key for the volume’s total capacity in bytes (read-only).
+            ------------------------------------------------------------------------    
+
+            In relation to Available/purgeable which macOS shows in info right click menu of the volume:
+            volumeAvailableCapacity -- Available disk space without purgeable
+            volumeAvailableCapacityForImportantUsage -- Available disk space + purgeable space
+            */
             if let capacity = values.volumeAvailableCapacity{
                 print("Available capacity: \(capacity)")
             } else {
